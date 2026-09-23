8e85c7e76b94a1ab8353f37b95f95a11819c4426
tkki_iOSX <tkki_iOSX@icloud.com>
2026-09-23 16:34:40 +0900

    Widgetを追加

---
+++ DigitalAnalogClock2Widget/DigitalAnalogClock2WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DigitalAnalogClock2WidgetBundle.swift
+//  DigitalAnalogClock2Widget
+//
+//  Created by Xcode2021 on 2026/09/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DigitalAnalogClock2WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DigitalAnalogClock2Widget()
+    }
+}
